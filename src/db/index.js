2b39e0a9ec94aae0c5a5cfd8690c2cd3fd24dc63
Robert Hovhannisyan <robhov97@gmail.com>
2023-06-18 21:50:48 +0400

    switched to dot env

--- src/db/index.js
+++ src/db/index.js
@@ -3,13 +3,13 @@ import { getDatabase } from "firebase/database";
 
 // TODO: conceal it or something
 const firebaseConfig = {
-  apiKey: "AIzaSyDADXDp3DthuikO4MH9JclzC97Yj0uksPw",
-  authDomain: "paig-68eab.firebaseapp.com",
-  databaseURL: "https://paig-68eab-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "paig-68eab",
-  storageBucket: "paig-68eab.appspot.com",
-  messagingSenderId: "599601281811",
-  appId: "1:599601281811:web:ff4c1d4448808a7854b8e6"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
